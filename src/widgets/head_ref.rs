40f1878371f57a2a7c4c64e37dc0c5ac547cbf6f
Rylan Polster <rslpolster@gmail.com>
2026-03-07 20:07:09 -0500

    Update head widgit

--- src/widgets/head_ref.rs
+++ src/widgets/head_ref.rs
@@ -5,6 +5,14 @@ pub struct HeadRef;
 
 impl Widget for HeadRef {
     fn content(&self, context: &Context) -> Option<String> {
-        Some(context.git.head()?.to_string())
+        if !context.git.is_git_repo() {
+            return None;
+        }
+
+        let Some(head) = context.git.head() else {
+            return Some("???".to_string());
+        };
+
+        Some(head.to_string())
     }
 }
