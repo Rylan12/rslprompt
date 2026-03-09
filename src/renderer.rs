a2f439d7b006278985f4449b6db58643dea4ce31
Rylan Polster <rslpolster@gmail.com>
2026-03-09 16:37:04 -0400

    Fix rendering of empty elements

--- src/renderer.rs
+++ src/renderer.rs
@@ -6,21 +6,19 @@ pub fn render_prompt(widgets: &[&dyn Widget], context: &Context) -> String {
     let mut space_allowed = false;
 
     for widget in widgets {
-        let mut rendered = render_widget(*widget, context);
+        let Some(content) = widget.content(context) else {
+            continue;
+        };
+
+        let color = widget.color(context);
+        let rendered = format!("{}{}", color.activate(), content);
 
         if space_allowed && widget.space_before() {
-            rendered = format!(" {}", rendered);
+            output.push(' ');
         }
-
         output.push_str(&rendered);
         space_allowed = widget.allow_space_after();
     }
 
     format!("{}{} ", output, Color::reset())
-}
-
-fn render_widget(widget: &dyn Widget, context: &Context) -> String {
-    let content = widget.content(context).unwrap_or_default();
-    let color = widget.color(context);
-    format!("{}{}", color.activate(), content)
 }
